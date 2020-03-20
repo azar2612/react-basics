64f785f5305b943bf4580d16dc5a257ca04721ae
azar <asarudeen26121997@gmail.com>
2020-03-20 11:59:57 +0530

    components in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
 import React from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <p>hey</p>
-    </div>
-  );
-}
+const Red = () => (
+  <div className="red">
+    <p>RED</p>
+  </div>
+);
+
+const Blue = () => (
+  <div className="blue">
+    <p>BLUE</p>
+  </div>
+);
+
+const Green = () => (
+  <div className="green">
+    <p>GREEN</p>
+  </div>
+);
+
+const App = () => (
+  <div className="app">
+    <p>hey</p>
+
+    <Red />
+    <Blue />
+    <Red />
+    <Green />
+  </div>
+);
 
 export default App;
