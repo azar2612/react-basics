7d7a8c2f0084c84007ab5b419c70cf0db6ed89cb
azar <asarudeen26121997@gmail.com>
2020-03-26 16:51:42 +0530

    photo toggle task completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import Color from "./color";
+import PhotoToggle from "./PhotoToggle";
 
 const App = () => (
   <div className="app">
@@ -10,6 +11,7 @@ const App = () => (
     <Color title="BLUE" />
     <Color title="YELLOW" />
     <Color title="GREEN" />
+    <PhotoToggle />
   </div>
 );
 
