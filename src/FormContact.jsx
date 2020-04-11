8a51588a4db521bfe57b85d7085990d96c297cd8
azar <asarudeen26121997@gmail.com>
2020-04-11 12:19:04 +0530

    contact task complete

---
+++ src/FormContact.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+
+class FormContact extends Component {
+  state = {
+    username: "",
+    password: "",
+    designation: "",
+    remember: false,
+    color: ""
+  };
+
+  handleChange = (e) => {
+    // console.log(e);
+    // console.log(e.target);
+    // console.log(e.target.value);
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  handleChecbox = (e) => {
+      this.setState({ [e.target.name]: e.target.checked})
+  }
+
+  handleSubmit = e => {
+      // prevent default stops page from reloading after pressing submit button
+      e.preventDefault();
+      console.log(this.state);
+  }
+
+  render() {
+    const { username, password, designation, remember, color} = this.state;
+    return (
+      <div className="form-contact">
+        <input name="username" value={username} type="text" placeholder="Enter your username" onChange={this.handleChange} />
+        <input name="password" value={password} type="password" placeholder="Enter your password" onChange={this.handleChange} />
+        <input name="designation" value={designation} type="text" placeholder="Enter your designation" onChange={this.handleChange} />
+        <input name="remember" checked={remember} type="checkbox" onChange={this.handleChecbox} />
+        <select name="color" value={color} onChange={this.handleChange} >
+          <option>red</option>
+          <option>blue</option>
+          <option>green</option>
+        </select>
+        <button type="button" onClick={this.handleSubmit}>submit</button>
+
+        <p>{username}</p>
+        <p>{password}</p>
+        <p>{designation}</p>
+        <p>{remember ? "true" : "false"}</p>
+        <p>{color}</p>
+      </div>
+    );
+  }
+}
+
+export default FormContact;
