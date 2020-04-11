78bfc6c0b22d72eaf986e0f2264a462707145f52
azar <asarudeen26121997@gmail.com>
2020-04-11 12:18:21 +0530

    form task completed

---
+++ src/Form.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import FormContact from "./FormContact";
+import FormValidate from "./FormValidate";
+import "./Form.css";
+const Form = () => (
+  <>
+    <FormContact />
+    <FormValidate />
+  </>
+);
+
+export default Form;
