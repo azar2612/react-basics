9dcd18b55e9ae11144e417625c1260e848371270
azar <asarudeen26121997@gmail.com>
2020-04-25 09:01:08 +0530

    local storage task updated

--- src/LocalStorage.jsx
+++ src/LocalStorage.jsx
@@ -1,6 +1,7 @@
-import React, { Component } from "react";
+import React from "react";
+import "./Localstorage.scss";
 
-class LocalStorage extends Component {
+class Localstorage extends React.Component {
   state = {
     todoText: "",
     data: "",
@@ -13,7 +14,6 @@ class LocalStorage extends Component {
     localStorage.setItem("todo", this.state.todoText);
     this.setState({ todoText: "" });
   };
-
   getData = () => {
     const cc = localStorage.getItem("todo");
     this.setState({ data: cc });
@@ -23,31 +23,39 @@ class LocalStorage extends Component {
     const { todoText, data } = this.state;
     return (
       <>
-        <form onSubmit={this.handleSubmit}>
-          <input
-            name="todoText"
-            type="text"
-            value={todoText}
-            onChange={this.handleChange}
-            placeholder="Enter Todo"
-          />
-          <button type="submit">Set data to Local Storage</button>
-        </form>
+        <div className="ls">
+          <div>
+            <form onSubmit={this.handleSubmit}>
+              <input
+                name="todoText"
+                type="text"
+                value={todoText}
+                onChange={this.handleChange}
+                placeholder="Enter Todo"
+              />
+              <button type="submit">set data to local storage </button>
+            </form>
+          </div>
 
-        <button type="button" onClick={this.getData}>
-          Get Data from localstorage
-        </button>
-        <p>{data && data}</p>
+          <div>
+            <button type="button" onClick={this.getData}>
+              get data to local storage{" "}
+            </button>
+            <p>{data && data}</p>
 
-        <button type="button" onClick={() => localStorage.removeItem("todo")}>
-          Delte Data from localstorage
-        </button>
-
-        <button type="button" onClick={() => localStorage.clear()}>
-          Clear Data from localstorage
-        </button>
+            <button
+              type="button"
+              onClick={() => localStorage.removeItem("todo")}
+            >
+              delete data to local storage{" "}
+            </button>
+            <button type="button" onClick={() => localStorage.clear()}>
+              clear data to local storage{" "}
+            </button>
+          </div>
+        </div>
       </>
     );
   }
 }
-export default LocalStorage;
+export default Localstorage;
