de9b856a173afa811835cc1a8b026ee835a2277e
azar <asarudeen26121997@gmail.com>
2020-05-05 10:20:00 +0530

    react task imported

--- src/App.jsx
+++ src/App.jsx
@@ -13,11 +13,19 @@ import Form from "./Form";
 import Validateform from "./Validateform";
 import Todo from "./Todo";
 import LiftState from "./LiftState";
+import Lifecycle from "./Lifecycle";
+import PropTypes from "./PropTypes";
+import CompoWrap from "./CompoWrap";
+import RouteExample from "./RouteExample";
+import { BlogPost, ArticlePost } from "./RouteExample/UrlParam";
+import CheckBoxEco from "./CheckBoxEco";
+import TestCase from "./TestCase";
 
 import LocalStorage from "./LocalStorage";
-import Redux from "./Redux";
-import Mobx from "./Mobx";
+
 import HooksBasic from "./HooksBasic";
+import CRUDwoHooks from "./CRUDwoHooks";
+import CRUDHooks from "./CRUDHooks";
 
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
@@ -61,9 +69,20 @@ const App = () => (
         <Route exact path="/LiftState" component={LiftState} />
         <Route exact path="/LocalStorage" component={LocalStorage} />
         <Route exact path="/Validateform" component={Validateform} />
-        <Route exact path="/Redux" component={Redux} />
-        <Route exact path="/Mobx" component={Mobx} />
+
         <Route exact path="/hooks-basic" component={HooksBasic} />
+        <Route exact path="/crud-wo-hooks" component={CRUDwoHooks} />
+        <Route exact path="/crud-hooks" component={CRUDHooks} />
+        <Route exact path="/lifecycle" component={Lifecycle} />
+        <Route exact path="/proptypes" component={PropTypes} />
+        <Route exact path="/compo-wrap" component={CompoWrap} />
+        <Route exact path="/route-example" component={RouteExample} />
+        <Route exact path="/route-example/nested" component={RouteExample} />
+        <Route exact path="/route-example/nested" component={RouteExample} />
+        <Route exact path="/url" component={BlogPost} />
+        <Route exact path="/a/:articlepost" component={ArticlePost} />
+        <Route exact path="/checkbox-eco" component={CheckBoxEco} />
+        <Route exact path="/test-case" component={TestCase} />
       </Switch>
     </BrowserRouter>
   </div>
